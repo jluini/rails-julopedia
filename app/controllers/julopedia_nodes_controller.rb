b581842c679dcf09a67ec2a8eff058f985aa7a4b
jluini <jluini@gmail.com>
2020-07-19 19:59:52 -0300

    Basic JulopediaNode CRUD

--- app/controllers/julopedia_nodes_controller.rb
+++ app/controllers/julopedia_nodes_controller.rb
@@ -4,5 +4,59 @@
 # Controller for creating, showing and editing JulopediaNode's.
 #
 class JulopediaNodesController < ApplicationController
-  def index; end
+  def index
+    @nodes = JulopediaNode.all
+  end
+  
+  def show
+    @node = JulopediaNode.find(params[:id])
+  end
+  
+  def new
+    @node = JulopediaNode.new_with_defaults(
+      title: 'Insert title here'
+    )
+  end
+  
+  def edit
+    @node = JulopediaNode.find(params[:id])
+  end
+  
+  def create
+    @node = JulopediaNode.new_with_defaults(node_params)
+     
+    if @node.save
+      redirect_to @node
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @node = JulopediaNode.find(params[:id])
+     
+    if @node.update(node_params)
+      redirect_to @node
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @node = JulopediaNode.find(params[:id])
+    @node.destroy
+   
+    redirect_to julopedia_nodes_path
+  end
+  
+  private
+  
+  def node_params
+    params.require(:julopedia_node).permit(
+      :name,
+      :kind,
+      :title,
+      :content
+    )
+  end
 end
--- app/helpers/julopedia_nodes_helper.rb
+++ app/helpers/julopedia_nodes_helper.rb
@@ -4,4 +4,11 @@
 # Helpers for JulopediaNodesController.
 #
 module JulopediaNodesHelper
+  def delete_link(node)
+    delete_url = julopedia_node_path(node)
+    link_to 'Destroy',
+            delete_url,
+            method: :delete,
+            data: { confirm: 'Are you sure?' }
+  end
 end
